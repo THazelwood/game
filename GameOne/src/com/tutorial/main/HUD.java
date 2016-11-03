df8a19ae0e3f424404e2e28c7c3477f36aaad701
THazelwood <Travbot5000@gmail.com>
2016-11-02 21:22:59 -0500

    Color_Transition_HUD
    created a color transition for HUD

--- GameOne/src/com/tutorial/main/HUD.java
+++ GameOne/src/com/tutorial/main/HUD.java
@@ -4,15 +4,18 @@ import java.awt.Graphics;
 
 public class HUD {
     public static int HEALTH = 100;
+    private int greenValue = 255;
     
     public void tick(){
-        
         HEALTH = Game.clamp(HEALTH, 0, 200);
+        greenValue = Game.clamp(greenValue, 0, 255);
+        
+        greenValue = HEALTH*2;
     }
     public void render(Graphics g){
         g.setColor(Color.gray);
         g.fillRect(15, 15, 200, 16);
-        g.setColor(Color.green);
+        g.setColor(new Color(75, greenValue, 0));
         g.fillRect(15, 15, HEALTH * 2, 16);
         g.setColor(Color.white);
         g.drawRect(15, 15, 200, 16);
